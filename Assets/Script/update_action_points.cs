74cbf2bac1f75c92b7fca8bc1e20d34721f12f2b
williamma621 <williamma621@gmail.com>
2024-12-15 21:32:29 -0800

    commit 3

--- Assets/Script/update_action_points.cs
+++ Assets/Script/update_action_points.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class update_action_points : MonoBehaviour
 {
     public TMP_Text action_point;
+    public TMP_Text personalText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,21 @@ public class update_action_points : MonoBehaviour
     void Update()
     {
         action_point.text = $"行动点：{static_variables.current_action_point}/365";
+        personalText.text = $@"
+        Name: Player
+        Age: {static_variables.current_year}/{static_variables.max_year}
+        Reputation: 
+        Morality: 
+        Cultivation Points: {static_variables.current_xp}
+        Knowledge: {static_variables.current_speed}
+        Realm: Qi Gathering Stage
+        Gold: 
+        Health: 
+        Attack: 
+        Defense: 
+        Mana: 
+
+
+        ";
     }
 }
