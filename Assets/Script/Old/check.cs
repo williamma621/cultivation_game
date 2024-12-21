b77c9950b18559c5c185db800eacb6912ec6dd13
lexus101 <89865282+lexus101@users.noreply.github.com>
2024-12-20 23:52:27 -0800

    1

---
+++ Assets/Script/Old/check.cs
@@ -0,0 +1,23 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check : MonoBehaviour
+{
+    public Variables variables;
+    public void checkdays()
+    {
+        
+      if (variables.current_action_point >= 365)
+      {
+            variables.current_action_point -= 365;
+            variables.current_year += 1;
+
+       }
+
+
+        
+    }
+}
+*/
